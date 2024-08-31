55402a44e1158393e3755d4f9385e814dd45c011
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-08-31 01:14:51 -0400

    Set up file based caching (for development)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cache settings
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": BASE_DIR / "cache",
+    }
+}
