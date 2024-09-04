d99ffa1c718fad1c7f782d3b774c6f25cefa71d1
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-09-04 09:09:33 -0400

    Add tests for filters.

--- backend/courses/tests/test_scheduling.py
+++ backend/courses/tests/test_scheduling.py
@@ -4,6 +4,7 @@ from django.core.management import call_command
 from courses.scheduling.time_bitmap import TimeBitmap
 from courses.scheduling.scheduling import get_valid_section_combinations, generate_schedules
 from courses.models import Section
+from courses.scheduling.filtering import is_section_downtown, is_section_before, is_section_after, is_section_closed
 
 
 class TestTimeBitmap(TestCase):
@@ -74,3 +75,46 @@ class TestScheduling(TestCase):
 
         combinations = get_valid_section_combinations("202309", "CSCI2000U")
         self.assertEqual(len(combinations), 7)
+
+
+
+class TestFiltering(TestCase):
+
+    def setUp(self) -> None:
+        call_command("updatesections", "202309", "--usecache")
+
+
+    def test_is_section_downtown(self):
+            
+        section = Section.objects.get(term="202309", course_reference_number="40424")
+        self.assertTrue(is_section_downtown(section))
+    
+        section = Section.objects.get(term="202309", course_reference_number="43546")
+        self.assertFalse(is_section_downtown(section))
+
+
+    def test_is_section_before(self):
+
+        section = Section.objects.get(term="202309", course_reference_number="40291")
+        self.assertTrue(is_section_before(section, "0900"))
+    
+        section = Section.objects.get(term="202309", course_reference_number="40288")
+        self.assertFalse(is_section_before(section, "0900"))
+
+
+    def test_is_section_after(self):
+
+        section = Section.objects.get(term="202309", course_reference_number="40291")
+        self.assertFalse(is_section_after(section, "0940"))
+
+        section = Section.objects.get(term="202309", course_reference_number="40288")
+        self.assertTrue(is_section_after(section, "1210"))
+
+
+    def test_is_section_closed(self):
+
+        section = Section.objects.get(term="202309", course_reference_number="40372")
+        self.assertFalse(is_section_closed(section))
+    
+        section = Section.objects.get(term="202309", course_reference_number="40371")
+        self.assertTrue(is_section_closed(section))
