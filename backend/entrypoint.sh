09356aaec66bf8b96264f44c39edd4973e7fd1fd
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-09-14 12:19:21 -0400

    Create superusers using just an email in `entrypoint.sh`

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 APP_PORT=${PORT:-8000}
 
@@ -8,6 +7,6 @@ cd /app/
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --noinput || true
+python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 
 gunicorn --worker-tmp-dir /dev/shm config.wsgi:application --bind "0.0.0.0:${APP_PORT}"
