765dead72cba852ba9c98b0ba59000438869043d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-10-13 04:39:20 -0400

    Include meeting times in SectionSerializer

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -1,20 +1,39 @@
 from rest_framework import serializers
 from .models import Course, Section, Term
+from .time_bitmap import TimeBitmap
 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ["subject", "subject_description", "subject_course", "course_title", "course_number"]
 
 
 class SectionSerializer(serializers.ModelSerializer):
+    meeting_times = serializers.SerializerMethodField()
     class Meta:
         model = Section
-        fields = "__all__"
+        fields = ["id", "course_reference_number", "course", "term", "schedule_type_description", "meeting_times"]
+
+
+    def get_meeting_times(self, obj: Section) -> list[dict]:
+        return [
+            {
+                "begin_time": meeting["meetingTime"]["beginTime"],
+                "end_time": meeting["meetingTime"]["endTime"],
+                "start_date": meeting["meetingTime"]["startDate"],
+                "end_date": meeting["meetingTime"]["endDate"],
+                "days": [
+                    day for day in TimeBitmap.DAYS if meeting["meetingTime"][day]
+                ],
+            }
+            for meeting in obj.meetings_faculty
+        ]
+
+
 
 
 class TermSerializer(serializers.ModelSerializer):
     class Meta:
         model = Term
-        fields = "__all__"
+        fields = ["term", "term_desc", "registration_open"]
