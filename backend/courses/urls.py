100f0e9ebd87e5dceda63c0fce64ec36f6656bee
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-10-13 04:38:41 -0400

    Add view to list sections

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.CoursesView.as_view(), name="courses"),
     path("terms/", views.TermsView.as_view(), name="terms"),
+    path("<str:course>/sections/", views.SectionsView.as_view(), name="sections"),
 ]
--- backend/courses/views.py
+++ backend/courses/views.py
@@ -6,8 +6,8 @@ from rest_framework import generics
 from rest_framework import filters
 import django_filters.rest_framework
 
-from .models import Term, Course
-from .serializers import TermSerializer, CourseSerializer
+from .models import Term, Course, Section
+from .serializers import TermSerializer, CourseSerializer, SectionSerializer
 
 
 class TermsView(generics.ListAPIView):
@@ -30,4 +30,23 @@ class CoursesView(generics.ListAPIView):
         if term:
             queryset = queryset.filter(section__term__term=term).distinct()
         queryset = queryset[:20]
+        return queryset
+    
+
+class SectionsView(generics.ListAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = []
+
+    def get_queryset(self):
+        return Section.objects.filter(
+            course__subject_course=self.kwargs.get("course")
+        )
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        term = self.request.query_params.get("term")
+        if term:
+            queryset = queryset.filter(term__term=term)
         return queryset
