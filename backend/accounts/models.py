4c636076b8d5fb84073c426d6775fdfbe8875693
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-10-23 02:54:17 -0400

    Add a phone field to the User model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,27 +3,22 @@ from django.utils import timezone
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
+def clean_email(email):
+    return email.lower().strip()
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password):
-
-        if not email:
-            raise ValueError("Users must have an email address.")
-
-        user = self.model(email=email.lower())
+        user = self.model(email=clean_email(email))
         user.set_password(password)
-
         user.save()
         return user
 
-
     def create_superuser(self, email, password):
-
         user = self.create_user(email, password)
-
         user.is_superuser = True
         user.is_staff = True
-
         user.save()
         return user
     
@@ -31,7 +26,9 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     email_verified = models.BooleanField(default=False)
-    username = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    phone_verified = models.BooleanField(default=False)
+    username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = UserManager()
