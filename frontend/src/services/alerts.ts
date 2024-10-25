b85556f02e970274a5aeaaa9bc6dcde3cf20d1d3
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-10-25 16:07:37 -0400

    Add services to list and delete alert subscriptions

--- frontend/src/services/alerts.ts
+++ frontend/src/services/alerts.ts
@@ -2,9 +2,26 @@ import instance from "@/services/base";
 import type { Term, Section } from "@/types";
 
 
-export async function createAlertSubscription(term: Term["term"], courseReferenceNumbers: Section["course_reference_number"][]) {
-  return await instance.post("alerts/subscriptions", {
+export async function createAlertSubscriptions(term: Term["term"], courseReferenceNumbers: Section["course_reference_number"][]) {
+  return await instance.post<Section[]>("alerts/subscriptions", {
     term: term,
     course_reference_numbers: courseReferenceNumbers,
+  });
+}
+
+
+export async function listAlertSubscriptions(term?: Term["term"]) {
+  return await instance.get<Section[]>("alerts/subscriptions", {
+    params: { term },
+  });
+}
+
+
+export async function deleteAlertSubscriptions(term: Term["term"], courseReferenceNumbers: Section["course_reference_number"][]) {
+  return await instance.delete("alerts/subscriptions", {
+    data: {
+      term: term,
+      course_reference_numbers: courseReferenceNumbers,
+    },
   });
 }
