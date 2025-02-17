43ca237d9d8ad3ea286927a31d3c092a57d9824a
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-02-17 16:32:08 -0500

    Create directories for media and static files in setup-mounted-volumes.sh

--- backend/setup-mounted-volumes.sh
+++ backend/setup-mounted-volumes.sh
@@ -1,5 +1,7 @@
 # Set permissions for mounted volumes
+mkdir -p media
 sudo chown -R 1001:1001 media
 sudo chmod -R 755 media
+mkdir -p staticfiles
 sudo chown -R 1001:1001 staticfiles
 sudo chmod -R 755 staticfiles
