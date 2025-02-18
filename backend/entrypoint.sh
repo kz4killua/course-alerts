d750c27d95e36aff6c8abb600263541e74672053
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-02-17 22:51:22 -0500

    Update application setup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,6 +7,6 @@ cd /app/
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+python manage.py createsuperuser --email ${SUPERUSER_EMAIL} --noinput || true
 
 gunicorn --worker-tmp-dir /dev/shm config.wsgi:application --bind "0.0.0.0:${APP_PORT}"
---
+++ backend/setup-application.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+# Update the package list
+sudo apt update
+
+# Set up environment variables
+echo "Ensure that the environment variables are set in the .env file."
+echo "Press any key to continue..."
+read -n 1 -s
+chmod 600 .env
+
+# Load environment variables
+set -o allexport
+source .env
+set +o allexport
+
+# Start services
+sudo sh setup-mounted-volumes.sh
+sudo sh redeploy.sh
+
+# Set up DNS
+echo "Log into your domain registrar and create the following record:"
+echo "Type: A"
+echo "Name: api"
+echo "Value: <server-ip>"
+echo "Press any key to continue..."
+read -n 1 -s
+
+# Set up Nginx
+sudo apt install nginx
+envsubst < nginx.conf | sudo tee /etc/nginx/sites-available/${DOMAIN} > /dev/null
+sudo ln -sf /etc/nginx/sites-available/${DOMAIN} /etc/nginx/sites-enabled
+sudo nginx -t
+sudo systemctl restart nginx
+
+# Set up Certbot
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d $DOMAIN
+sudo certbot renew --dry-run
