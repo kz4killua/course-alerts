efa1705cc0958dc826c25502a761eb34c404892c
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-02-17 22:09:06 -0500

    Parameterize user and group IDs in Dockerfile and setup-mounted-volumes.sh

--- backend/setup-mounted-volumes.sh
+++ backend/setup-mounted-volumes.sh
@@ -1,7 +1,11 @@
+# NOTE: Ensure these match the user and group IDs in the Dockerfile.
+UID=1001
+GID=1001
+
 # Set permissions for mounted volumes
 mkdir -p media
-sudo chown -R 1001:1001 media
+sudo chown -R $UID:$GID media
 sudo chmod -R 755 media
 mkdir -p staticfiles
-sudo chown -R 1001:1001 staticfiles
+sudo chown -R $UID:$GID staticfiles
 sudo chmod -R 755 staticfiles
