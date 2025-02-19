095fecdff8403d3e65aa39dede913e42fdaf0186
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-02-19 10:04:30 -0500

    Update setup-mounted-volumes.sh to avoid using reserved variables

--- backend/setup-mounted-volumes.sh
+++ backend/setup-mounted-volumes.sh
@@ -1,11 +1,11 @@
 # NOTE: Ensure these match the user and group IDs in the Dockerfile.
-UID=1001
-GID=1001
+DOCKERUSER_UID=1001
+DOCKERUSER_GID=1001
 
 # Set permissions for mounted volumes
 mkdir -p media
-sudo chown -R $UID:$GID media
+sudo chown -R $DOCKERUSER_UID:$DOCKERUSER_GID media
 sudo chmod -R 755 media
 mkdir -p staticfiles
-sudo chown -R $UID:$GID staticfiles
+sudo chown -R $DOCKERUSER_UID:$DOCKERUSER_GID staticfiles
 sudo chmod -R 755 staticfiles
