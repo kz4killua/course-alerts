ad9663d8938da9d86ad217f9b0b55435a98be5a8
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-07-14 16:23:03 -0400

    Update `SearchBar` and `Input` styles

--- frontend/src/components/shared/search-bar.tsx
+++ frontend/src/components/shared/search-bar.tsx
@@ -14,15 +14,15 @@ export function SearchBar({
 }) {
   return (
     <div className={clsx(
-      "group flex items-center justify-center",
-      "rounded-md border border-input px-3 py-1",
+      "bg-white group flex gap-x-1 items-center justify-center",
+      "rounded-md border border-input px-6 py-2",
       "shadow-sm transition-colors",
       "focus-within:ring-1 focus-within:ring-ring"
     )}>
       <SearchIcon size={16} />
       <Input 
         className={clsx(
-          "border-0 ring-0 shadow-none focus-visible:border-0 focus-visible:ring-0 placeholder:font-medium",
+          "border-0 ring-0 shadow-none focus-visible:border-0 focus-visible:ring-0",
         )}
         type="text" 
         placeholder={placeholder} 
