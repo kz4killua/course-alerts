5cd0c38eee2c17de5fdfffa27c9fbe15707e50c8
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-07-18 00:06:46 -0400

    Update `PageLayout` minimum height

--- frontend/src/components/shared/page-layout.tsx
+++ frontend/src/components/shared/page-layout.tsx
@@ -9,14 +9,12 @@ export function PageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <div className="flex flex-col min-h-dvh">
       <Header />
-      <Container className="flex flex-col min-h-screen">
-        <div className="grow">
-          { children }
-        </div>
+      <Container className="grow w-full">
+        { children }
       </Container>
-      <Container>
+      <Container className="w-full">
         <Footer />
       </Container>
     </div>
