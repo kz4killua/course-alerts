3817d48024bc9fe123591d8184906f960321940c
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-22 19:36:29 -0400

    Disable flex shrinking on drawer "handle"

--- frontend/src/components/ui/drawer.tsx
+++ frontend/src/components/ui/drawer.tsx
@@ -48,7 +48,12 @@ const DrawerContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className={cn(
+        "mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted",
+        // Customizations
+        "shrink-0",
+        )} 
+      />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
