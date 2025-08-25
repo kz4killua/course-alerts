1576ad5682526789419d0de97096d32910c91589
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-25 12:21:17 -0400

    Add `--no-sync` option to all `uv run` commands

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-uv run python manage.py migrate --noinput
-uv run python manage.py collectstatic --noinput
-uv run python manage.py createsuperuser --email ${DJANGO_SUPERUSER_EMAIL} --noinput || true
-exec uv run gunicorn --worker-tmp-dir /dev/shm config.wsgi:application --bind "0.0.0.0:${PORT}"
+uv run --no-sync python manage.py migrate --noinput
+uv run --no-sync python manage.py collectstatic --noinput
+uv run --no-sync python manage.py createsuperuser --email ${DJANGO_SUPERUSER_EMAIL} --noinput || true
+exec uv run --no-sync gunicorn --worker-tmp-dir /dev/shm config.wsgi:application --bind "0.0.0.0:${PORT}"
