0353fcb88e296e122d474b3c61de1325dbc771d3
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-12-21 07:57:25 -0500

    Update `UserAdmin`

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -7,8 +7,8 @@ from accounts.models import EmailVerificationCode, User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     ordering = ["email"]
-    list_display = ["email", "phone", "last_login"]
-    search_fields = ["email"]
+    list_display = ["email", "phone", "last_login", "date_joined"]
+    search_fields = ["email", "phone"]
     fieldsets = [
         ("Personal info", {"fields": ("email", "phone")}),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
