85288c82a23ee1d7cfa3c3636940f4d5198087af
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2026-04-28 03:06:07 -0400

    Set up `prettier`

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,14 @@
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
+import prettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
   ...nextVitals,
+  prettier,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
 ])
